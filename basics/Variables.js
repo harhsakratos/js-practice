07d4f23db1df6b339b8ed90089e6575e738b771b
HarshaKratos <75169126+harhsakratos@users.noreply.github.com>
2023-10-03 16:25:21 +0000

    Variables

---
+++ basics/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = 'kratos@gos.com'
+var accountPassword = '12345'
+accountCity = 'sparta'
+
+// accountId = 2
+
+accountEmail = 'athena@athens.com'
+accountPassword = 'pandoras box'
+accountCity = 'Athens'
+
+console.table[accountEmail,accountPassword,accountCity]
