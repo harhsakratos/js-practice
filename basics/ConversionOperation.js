d68f631520360b82e8ac2b0ee8af8a116e8219b8
HarshaKratos <75169126+harhsakratos@users.noreply.github.com>
2023-10-04 11:25:06 +0000

    data type practice

--- basics/ConversionOperation.js
+++ basics/ConversionOperation.js
@@ -6,6 +6,14 @@
 // console.log(`The Value in number after conversion is ${typeof(ValueInNumber)}`)
 // console.log(ValueInNumber);
 
-let a = ""
-let b = Boolean(a)
-console.log(b)
+// let a = ""
+// let b = Boolean(a)
+// console.log(b)
+
+//*****************oprations**************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
