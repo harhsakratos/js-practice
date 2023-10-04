408fe4868652f8b8dbd5614f55cacf506ed8d138
HarshaKratos <75169126+harhsakratos@users.noreply.github.com>
2023-10-04 09:57:00 +0000

    data type conversion

---
+++ basics/ConversionOperation.js
@@ -0,0 +1,12 @@
+// let score = false
+
+// console.log(typeof(score))
+
+// let ValueInNumber = Number(score)
+// console.log(`The Value in number after conversion is ${typeof(ValueInNumber)}`)
+// console.log(ValueInNumber);
+
+let a = ""
+
+let b = Boolean(a)
+console.log(b)
