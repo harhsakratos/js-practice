a027bc2425d791ab77b5cd7beb866412183546f9
HarshaKratos <75169126+harhsakratos@users.noreply.github.com>
2023-10-04 15:52:04 +0000

    practicing data types

---
+++ basics/DataTypes-Summary.js
@@ -0,0 +1,64 @@
+//primitive and non primitive
+
+//primitive has 7 types - string , number , boolean , null , undefined , symbol , bigint(append (n) after the value , just like we did it in java with float)
+
+
+
+// let a = "harsha"
+// let b = 1
+// let c = true
+// let d = undefined
+// let e = Symbol('123')
+// let f = 123456789n
+// let g = null
+
+// let value1 = typeof(a)
+// let value2 = typeof(b)
+// let value3 = typeof(c)
+// let value4 = typeof(d)
+// let value5 = typeof(e)
+// let value6 = typeof(f)
+// let value7 = typeof(g)
+
+// console.log(value1)
+// console.log(value2)
+// console.log(value3)
+// console.log(value4)
+// console.log(value5)
+// console.log(value6)
+// console.log(value7)
+
+//non primitive - array , object ,  functions
+
+
+
+let user = {
+    firstname: "harsha",
+    age: 24,
+    token: true
+}
+
+let array = [1, 2, 3, 4, 5]
+
+let RoughFunction = MyFunction = () => {
+    let i = 0
+    array.map((value) => {
+        i = i + value
+    })
+    console.log(i)
+}
+
+MyFunction()
+
+/*
+value = 1
+i = 0
+i = 0+1 = 1
+
+*/
+
+
+// let a = array[0]
+// console.log(a)
+// array[4] = 100
+// console.log(array)
